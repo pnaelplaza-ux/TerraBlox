b78c384ace22d93e601cdcfc7b8a59af83e7069f
pnaelplaza-ux <pnaelplaza@gmail.com>
2026-01-04 20:28:04 +0100

    Create BiomeDetails.tsx

---
+++ components/BiomeDetails.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// This component has been deprecated as per user request to remove trees and rocks.
+// Keeping the file as a placeholder to avoid build errors if referenced elsewhere (though removed from Scene).
+export const BiomeDetails: React.FC<any> = () => {
+  return null;
+};
